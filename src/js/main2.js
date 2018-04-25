65568ea20d35f59ecccae9d29eff3c7f6e6258bd
roderick <rod@nete.com>
2018-04-25 17:49:47 -0400

    First page done

--- src/js/main2.js
+++ src/js/main2.js
@@ -11,7 +11,7 @@ $(function() {
   });
 });
 
-$(document).on('click', '[data-toggle="lightbox]', function(event){
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
